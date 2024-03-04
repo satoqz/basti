b9e2a0e0808e97afe78bc4c9bc820bff904ecb1d
satoqz <mail@satoqz.net>
2024-03-04 21:10:55 +0100

    bastictl: check if `watch` was not in PATH

--- basti-client/src/util.rs
+++ basti-client/src/util.rs
@@ -1,4 +1,8 @@
-use std::{collections::HashSet, env, hash::Hash, os::unix::process::CommandExt, process::Command};
+use anyhow::anyhow;
+use std::{
+    collections::HashSet, env, hash::Hash, io::ErrorKind, os::unix::process::CommandExt,
+    process::Command,
+};
 
 pub fn reexec_with_watch(interval: f32) -> anyhow::Result<()> {
     let args = ["--color", "--no-rerun", "--no-title", "--no-wrap"]
@@ -7,7 +11,11 @@ pub fn reexec_with_watch(interval: f32) -> anyhow::Result<()> {
         .chain([format!("--interval={interval}"), "--exec".into()])
         .chain(env::args().filter(|arg| arg != "--watch"));
 
-    Err(Command::new("watch").args(args).exec().into())
+    let err = Command::new("watch").args(args).exec();
+    Err(match err.kind() {
+        ErrorKind::NotFound => anyhow!("`watch` utility not found in PATH"),
+        _ => err.into(),
+    })
 }
 
 pub trait Compact {
