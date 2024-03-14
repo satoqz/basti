b6a3bd2e0540f3892177054b741994f10be2c8df
satoqz <mail@satoqz.net>
2024-03-14 19:57:29 +0100

    fix typo

--- basti-client/src/main.rs
+++ basti-client/src/main.rs
@@ -19,7 +19,7 @@ struct Cli {
         env = "BASTICTL_CLUSTER",
         default_value = "http://127.0.0.1:1337",
         use_value_delimiter = true,
-        help = "Comma-separeted list of cluster endpoints"
+        help = "Comma-separated list of cluster endpoints"
     )]
     cluster: Vec<Url>,
 }
